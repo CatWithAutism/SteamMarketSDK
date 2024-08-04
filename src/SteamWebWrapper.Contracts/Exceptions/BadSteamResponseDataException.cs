6b666208a37cc3b82180875562edce7648e4d637
Vladislav Knyazev <vlad356102@yandex.ru>
2024-08-04 03:28:18 +0300

    Added new Exception

---
+++ src/SteamWebWrapper.Contracts/Exceptions/BadSteamResponseDataException.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+using System.Text.Json;
+
+namespace SteamWebWrapper.Contracts.Exceptions;
+
+public class BadSteamResponseDataException : JsonException
+{
+	public BadSteamResponseDataException()
+	{
+	}
+
+	protected BadSteamResponseDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+	{
+	}
+
+	public BadSteamResponseDataException(string? message) : base(message)
+	{
+	}
+
+	public BadSteamResponseDataException(string? message, Exception? innerException) : base(message, innerException)
+	{
+	}
+
+	public BadSteamResponseDataException(string? message, string? path, long? lineNumber, long? bytePositionInLine) : base(message, path, lineNumber, bytePositionInLine)
+	{
+	}
+
+	public BadSteamResponseDataException(string? message, string? path, long? lineNumber, long? bytePositionInLine, Exception? innerException) : base(message, path, lineNumber, bytePositionInLine, innerException)
+	{
+	}
+}
